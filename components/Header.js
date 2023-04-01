f3ae43b8a3a59c1fc02b4b229f48258453d35d8b
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-04-01 00:20:38 -0700

    Turn each into link

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,28 @@
+import Link from 'next/link'
+
 const Header = () => {
   return (
     <div className='w-full flex justify-between py-12 px-32'>
-      <p className='text-xl'>Insert logo here</p>
-      <div className='flex justify-between items-center text-md font-bold uppercase w-[50%]'>
-        <p>Work</p>
-        <p>Services</p>
-        <p>Solutions</p>
-        <p>Company</p>
-        <p>Contact</p>
-      </div>
+      <p className='text-lg'>
+        <Link href='/'>Insert logo here</Link>
+      </p>
+      <ul className='flex justify-between items-center text-md font-bold uppercase w-[50%]'>
+        <li>
+          <Link href='/'>Work</Link>
+        </li>
+        <li>
+          <Link href='/'>Services</Link>
+        </li>
+        <li>
+          <Link href='/'>Solutions</Link>
+        </li>
+        <li>
+          <Link href='/'>Company</Link>
+        </li>
+        <li>
+          <Link href='/'>Contact</Link>
+        </li>
+      </ul>
     </div>
   )
 }
