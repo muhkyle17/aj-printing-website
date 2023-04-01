17fade6d9e787fe4746903b3db32bb070adce2c2
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-04-01 00:06:36 -0700

    Initialize the look of the header

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,14 @@
 const Header = () => {
   return (
-    <div>
-      <p>Header</p>
+    <div className='w-full flex justify-between py-12 px-32'>
+      <p className='text-xl'>Insert logo here</p>
+      <div className='flex justify-between items-center text-md font-bold uppercase w-[50%]'>
+        <p>Work</p>
+        <p>Services</p>
+        <p>Solutions</p>
+        <p>Company</p>
+        <p>Contact</p>
+      </div>
     </div>
   )
 }
