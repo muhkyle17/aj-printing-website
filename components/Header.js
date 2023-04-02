fa270a103afd37408c6fe44e331331fbcd8976af
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-04-02 01:47:08 -0700

    Add a container and mx-auto property

--- components/Header.js
+++ components/Header.js
@@ -2,28 +2,30 @@ import Link from 'next/link'
 
 const Header = () => {
   return (
-    <div className='w-full flex justify-between py-12 px-32'>
-      <p className='text-lg'>
-        <Link href='/'>Insert logo here</Link>
-      </p>
-      <ul className='flex justify-between items-center text-base font-bold uppercase w-[40%]'>
-        <li>
-          <Link href='/'>Work</Link>
-        </li>
-        <li>
-          <Link href='/'>Services</Link>
-        </li>
-        <li>
-          <Link href='/'>Solutions</Link>
-        </li>
-        <li>
-          <Link href='/'>Company</Link>
-        </li>
-        <li>
-          <Link href='/'>Contact</Link>
-        </li>
-      </ul>
-    </div>
+    <section className='container mx-auto'>
+      <div className='flex justify-between py-12 px-4'>
+        <p className='text-lg'>
+          <Link href='/'>Insert logo here</Link>
+        </p>
+        <ul className='flex justify-between items-center text-base font-bold uppercase w-[50%]'>
+          <li>
+            <Link href='/'>Work</Link>
+          </li>
+          <li>
+            <Link href='/'>Services</Link>
+          </li>
+          <li>
+            <Link href='/'>Solutions</Link>
+          </li>
+          <li>
+            <Link href='/'>Company</Link>
+          </li>
+          <li>
+            <Link href='/'>Contact</Link>
+          </li>
+        </ul>
+      </div>
+    </section>
   )
 }
 
