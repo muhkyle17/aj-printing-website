8c76261b0c20bb322b8a29f18d1fb5f7c329ce3d
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-04-02 23:03:27 -0700

    Lessen padding on the top

--- components/common/Header.js
+++ components/common/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
     <header
       className={`${
         bg ? 'bg-tertiary h-20' : 'h-24'
-      } items-center fixed top-0 w-full z-10 transition-all duration-300 py-12 px-10 md:px-7 lg:px-0`}>
+      } items-center fixed top-0 w-full z-10 transition-all duration-300 py-8 px-10 md:px-7 lg:px-0`}>
       <div className='flex justify-between text-white transition-all duration-300 container mx-auto'>
         <h1 className='name text-5xl hover:text-primary transition-all duration-300'>
           <Link href='/'>Insert logo here</Link>
