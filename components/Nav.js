4cde405c449225b0492b9291d3d29165fec2a046
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-04-02 22:41:47 -0700

    Fix structure of the header to include styling of nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,24 @@
+import Link from 'next/link'
+
 const Nav = () => {
   return (
-    <div>
-      <p>Nav bar</p>
-    </div>
+    <ul className='flex justify-between items-center text-base font-bold uppercase md:w-[60%] lg:w-[40%]'>
+      <li>
+        <Link href='/'>Work</Link>
+      </li>
+      <li>
+        <Link href='/'>Services</Link>
+      </li>
+      <li>
+        <Link href='/'>Solutions</Link>
+      </li>
+      <li>
+        <Link href='/'>Company</Link>
+      </li>
+      <li>
+        <Link href='/'>Contact</Link>
+      </li>
+    </ul>
   )
 }
 
--- components/common/Header.js
+++ components/common/Header.js
@@ -1,31 +1,23 @@
 import Link from 'next/link'
+import { useState } from 'react'
+
+import Nav from '../Nav'
 
 const Header = () => {
+  const [bg, setBg] = useState(false)
+
   return (
-    <section className='container mx-auto'>
-      <div className='flex justify-between py-12 px-4'>
+    <header
+      className={`${
+        bg ? 'bg-tertiary h-20' : 'h-24'
+      } items-center fixed top-0 w-full z-10 transition-all duration-300 py-12 px-10 md:px-7 lg:px-0`}>
+      <div className='flex justify-between text-white transition-all duration-300 container mx-auto'>
         <p className='text-lg'>
           <Link href='/'>Insert logo here</Link>
         </p>
-        <ul className='flex justify-between items-center text-base font-bold uppercase md:w-[60%] lg:w-[40%]'>
-          <li>
-            <Link href='/'>Work</Link>
-          </li>
-          <li>
-            <Link href='/'>Services</Link>
-          </li>
-          <li>
-            <Link href='/'>Solutions</Link>
-          </li>
-          <li>
-            <Link href='/'>Company</Link>
-          </li>
-          <li>
-            <Link href='/'>Contact</Link>
-          </li>
-        </ul>
+        <Nav />
       </div>
-    </section>
+    </header>
   )
 }
 
