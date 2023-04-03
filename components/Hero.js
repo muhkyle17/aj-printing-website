ee4c0c16c9caf4cdc652b12eff14594fc6f52bf5
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-04-02 23:56:46 -0700

    Adjust placement of hero text

--- components/Hero.js
+++ components/Hero.js
@@ -6,10 +6,10 @@ const Hero = () => {
   return (
     <section className='h-[100vh] flex items-center lg:bg-cover lg:bg-center lg:bg-no-repeat py-32 lg:py-0 overflow-hidden relative'>
       <div className='h-[80%] container mx-auto flex items-end'>
-        <div className='z-10'>
-          <p className='text-5xl'>Intuitive design.</p>
-          <p className='text-5xl'>Engineering Power</p>
-          <p className='text-5xl'>
+        <div className='flex flex-col pl-[20rem] pb-[10rem] gap-2 z-10'>
+          <p className='text-7xl'>Intuitive design.</p>
+          <p className='text-7xl font-extrabold'>Engineering Power.</p>
+          <p className='text-xl mt-5'>
             We design & build extraordinary websites and mobile apps of all sizes.
           </p>
         </div>
