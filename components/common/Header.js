26791cee0263ceb693c12ddcb61b4ddc5aed28f7
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-04-03 00:09:52 -0700

    Fix header styling

--- components/common/Header.js
+++ components/common/Header.js
@@ -10,8 +10,8 @@ const Header = () => {
     <header
       className={`${
         bg ? 'bg-tertiary h-20' : 'h-24'
-      } items-center fixed top-0 w-full z-50 transition-all duration-300 py-8// px-10 md:px-7 lg:px-0`}>
-      <div className='flex justify-between text-white transition-all duration-300 container mx-auto'>
+      } flex items-center fixed top-0 w-full z-50 transition-all duration-300 py-8// px-10 md:px-7 lg:px-0`}>
+      <div className='flex items-center justify-between text-white transition-all duration-300 container mx-auto'>
         <h1 className='name text-5xl hover:text-primary transition-all duration-300'>
           <Link href='/'>Insert logo here</Link>
         </h1>
