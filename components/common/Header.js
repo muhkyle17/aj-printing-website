c2c731472bde99f5c4b47277d4364de77ed41d8d
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-04-03 18:19:41 -0700

    Change to name of company

--- components/common/Header.js
+++ components/common/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
       } flex items-center fixed top-0 w-full z-50 transition-all duration-300 py-8// px-10 md:px-7 lg:px-0`}>
       <div className='flex items-center justify-between text-white transition-all duration-300 container mx-auto'>
         <h1 className='name text-5xl hover:text-primary transition-all duration-300'>
-          <Link href='/'>Insert logo here</Link>
+          <Link href='/'>North Graphic Supplies</Link>
         </h1>
         <Nav />
       </div>
