cc535843660316560d459151bc100282a32140ba
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-04-04 19:44:23 -0700

    Add another layer of responsiveness

--- components/Hero.js
+++ components/Hero.js
@@ -5,15 +5,17 @@ import HeroImage from '../public/hero-printing.jpeg'
 const Hero = () => {
   return (
     <section className='h-[100vh] flex items-center py-32 lg:py-0 overflow-hidden relative'>
-      <div className='lg:absolute z-10 px-24 lg:bottom-[17rem] lg:right-[22rem] flex flex-col gap-2'>
-        <p className='text-4xl md:text-6xl xl:text-7xl'>Intuitive design.</p>
-        <p className='text-4xl md:text-6xl xl:text-7xl font-extrabold'>Engineering Power.</p>
+      <div className='lg:absolute z-10 px-24 container w-full sm:w-auto lg:bottom-[17rem] lg:right-[22rem] flex flex-col gap-2 text-secondary text-center md:text-left'>
+        <p className='text-2xl sm:text-4xl md:text-6xl xl:text-7xl'>Intuitive design.</p>
+        <p className='text-2xl sm:text-4xl md:text-6xl xl:text-7xl font-extrabold'>
+          Engineering Power.
+        </p>
         <p className='text-md md:text-lg xl:text-xl mt-5'>
           We design & build extraordinary websites and mobile apps of all sizes.
         </p>
       </div>
       <div
-        className='fixed w-full lg:w-[800px] h-[67%] rounded-sm bottom-20 right-0 bg-cover bg-center bg-no-repeat'
+        className='fixed w-full lg:w-[800px] h-[70vh] rounded-lg bottom-20 right-0 bg-cover bg-center bg-no-repeat opacity-50 lg:opacity-80'
         style={{
           backgroundImage: `url('/hero-printing.jpeg')`,
         }}
