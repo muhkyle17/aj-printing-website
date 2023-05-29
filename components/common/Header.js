b83ca8f9689c0fa404a401da73cbac1a910532d3
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-05-29 00:41:16 -0700

    Add lora font for north graphic suppplies 'logo'

--- components/common/Header.js
+++ components/common/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
         bg ? 'bg-tertiary h-20' : 'h-32'
       } flex items-center fixed top-0 w-full z-50 transition-all duration-300 px-10 md:px-7 lg:px-0`}>
       <div className='flex items-center gap-10 justify-between text-primary transition-all duration-300 container mx-auto'>
-        <h1 className='name hidden sm:block text-5xl hover:text-black transition-all duration-300'>
+        <h1 className='name hidden sm:block text-4xl font-medium hover:text-black transition-all duration-300'>
           <Link href='/'>North Graphic Supplies</Link>
         </h1>
         <h1 className='name block sm:hidden text-3xl hover:text-black transition-all duration-300'>
