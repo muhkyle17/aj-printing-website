8fac35f4aedbe751adb8da201d44bcfb24be8894
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-01 00:33:27 -0700

    Add functionality to show dropdown on hover

--- components/Nav.js
+++ components/Nav.js
@@ -1,6 +1,11 @@
 import Link from 'next/link'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [showProductsDropdown, setShowProductsDropdown] = useState(false)
+
+  console.log(showProductsDropdown)
+
   const baseStyles = 'hover:text-black transition-all duration-300'
 
   return (
@@ -11,11 +16,33 @@ const Nav = () => {
       <li className={`${baseStyles}`}>
         <Link href='/company'>Company</Link>
       </li>
-      <li className={`${baseStyles}`}>
-        <Link href='/products'>Products</Link>
+      <li className={`${baseStyles} relative`}>
+        <Link
+          href='/products'
+          className='dropdown-hover'
+          onMouseEnter={() => setShowProductsDropdown(true)}
+          onMouseLeave={() => setShowProductsDropdown(false)}>
+          Products
+        </Link>
+        {showProductsDropdown && (
+          <div className='absolute flex flex-col'>
+            <Link href='/'>Roll to roll labels</Link>
+            <Link href='/'>Sheeted Labels</Link>
+            <Link href='/'>In Mould Labels</Link>
+          </div>
+        )}
       </li>
       <li className={`${baseStyles}`}>
-        <Link href='/markets'>Market</Link>
+        <Link href='/markets' className='dropdown-hover'>
+          Market
+        </Link>
+        <div className='flex flex-col'>
+          <Link href='/'>B&F</Link>
+          <Link href='/'>Healthcare</Link>
+          <Link href='/'>Promotions</Link>
+          <Link href='/'>Barcodes</Link>
+          <Link href='/'>Chemicals</Link>
+        </div>
       </li>
       <li className={`${baseStyles}`}>
         <Link href='/contact'>Contact</Link>
