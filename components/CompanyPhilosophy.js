420166015f016530ef9cbd2d526bd7ff3711c9ef
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-02 01:24:30 -0700

    Slightly adjust styling of the company value

--- components/CompanyPhilosophy.js
+++ components/CompanyPhilosophy.js
@@ -1,10 +1,10 @@
 const CompanyPhilosophy = () => {
   return (
     <div className='h-[100vh] flex flex-col items-center container mx-auto'>
-      <div className='flex flex-col gap-3'>
+      <div className='flex flex-col'>
         <p className='text-5xl text-primary opacity-80'>Our</p>
-        <p className='text-8xl pl-10 text-[#43759b]'>Company</p>
-        <p className='text-8xl pl-52 text-[#264D6A]'>Values</p>
+        <p className='text-8xl pl-10 mb-5 text-[#43759b]'>Company</p>
+        <p className='text-7xl pl-52 pl-60 text-[#264D6A]'>Values</p>
       </div>
     </div>
   )
