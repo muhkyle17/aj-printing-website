64a0bca85104cf1a360166190763b27bc151cfa0
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-03 13:06:37 -0700

    Add reponsiveness to company philosophy main content

--- components/CompanyPhilosophy.js
+++ components/CompanyPhilosophy.js
@@ -4,20 +4,30 @@ import printingImage2 from '../public/print-image-2.jpeg'
 
 const CompanyPhilosophy = () => {
   return (
-    <div className='h-[100vh] flex flex-col gap-20 items-center container mx-auto'>
+    <div className='h-full flex flex-col gap-20 items-center container mx-auto'>
       <div className='flex flex-col'>
         <p className='text-5xl text-primary opacity-80'>Our</p>
         <p className='text-8xl pl-10 mb-5 text-[#43759b]'>Company</p>
         <p className='text-7xl pl-60 text-[#264D6A]'>Values</p>
       </div>
-      <div className='flex items-center gap-3 w-full mx-28'>
-        <p className='text-base mr-10 text-primary opacity-80 leading-6'>
+      <div className='flex xl:flex-row flex-col items-center gap-2 w-full xl:mx-28'>
+        <p className='text-base w-3/4 xl:w-1/4 text-primary opacity-80 leading-6'>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, officiis nostrum
           voluptatum iste quas mollitia explicabo nisi eveniet perferendis, cumque et ipsum
           consectetur laborum nulla facilis quam magnam nam placeat possimus saepe earum!
         </p>
-        <Image src={printingImage} alt='Print image' className='w-4/6 rounded-[50px] h-[25rem]' />
-        <Image src={printingImage2} alt='Print image' className='w-2/6 rounded-[50px] h-[25rem]' />
+        <div className='flex flex-wrap flex-col lg:flex-row lg:items-center w-3/4 mx-auto xl:flex-nowrap gap-5'>
+          <Image
+            src={printingImage}
+            alt='Print image'
+            className='w-auto rounded-[50px] h-auto md:h-[25rem]'
+          />
+          <Image
+            src={printingImage2}
+            alt='Print image'
+            className='w-auto rounded-[50px] self-start h-auto md:h-[25rem]'
+          />
+        </div>
       </div>
     </div>
   )
