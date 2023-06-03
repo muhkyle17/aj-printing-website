af88e7fe426c31877edb13996084d2bc1cf3ad56
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-02 23:49:48 -0700

    Add discover products header

--- components/Products.js
+++ components/Products.js
@@ -1,7 +1,13 @@
 const Products = () => {
   return (
-    <div>
-      <p>Discover Our Products</p>
+    <div className='h-[100vh]'>
+      <div className='flex flex-col gap-6 items-center container mx-auto'>
+        <div className='flex flex-col'>
+          <p className='text-5xl text-primary opacity-80'>Discover</p>
+          <p className='text-7xl pl-40 text-[#43759b]'>Our</p>
+          <p className='text-8xl pl-60 text-[#265D6A]'>Products</p>
+        </div>
+      </div>
     </div>
   )
 }
