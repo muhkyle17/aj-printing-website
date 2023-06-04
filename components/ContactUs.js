49c3698a1253c2c5e89f48abbcf069bfe662d89c
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-04 12:34:55 -0700

    Initialize contact form component -- still needs some tweaking because it's currently not working for some reason

--- components/ContactUs.js
+++ components/ContactUs.js
@@ -1,7 +1,14 @@
 const ContactUs = () => {
   return (
     <div>
-      <p>Contact Us</p>
+      {/* <form action='https://formsubmit.co/kylereyes_09@yahoo.com' method='POST'> */}
+      <form action='https://formsubmit.co/reyesmikyle17@gmail.com' method='POST'>
+        <input type='text' name='firstName' placeholder='First Name' required />
+        <input type='text' name='lastName' placeholder='Last Name' required />
+        <input type='text' name='company' placeholder='Company Name' required />
+        <input type='hidden' name='_captcha' value='false' />
+        <button type='submit'>Send</button>
+      </form>
     </div>
   )
 }
