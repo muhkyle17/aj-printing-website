863422568413215e363a6c03016dfe31baebece9
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-04 08:17:39 -0700

    Slightly adjust header spacing from the left

--- components/Products.js
+++ components/Products.js
@@ -5,7 +5,7 @@ const Products = () => {
     <div className='h-full flex flex-col gap-10 md:gap-20 items-center container mx-auto mb-[100px]'>
       <div className='flex flex-col items-center md:items-baseline'>
         <p className='text-5xl md:text-6xl text-primary opacity-80'>Discover</p>
-        <p className='text-5xl md:text-6xl md:pl-40 text-[#43759b]'>Our</p>
+        <p className='text-5xl md:text-6xl md:pl-48 text-[#43759b]'>Our</p>
         <p className='text-5xl md:text-8xl md:pl-60 text-[#265D6A]'>Products</p>
       </div>
       <div className='flex flex-col text-center'>
