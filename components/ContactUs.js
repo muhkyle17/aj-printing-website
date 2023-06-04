c322ee9b7efe6097c761a6c0c353b6c287646a28
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-04 15:48:21 -0700

    Add a little structure to the contact form styling

--- components/ContactUs.js
+++ components/ContactUs.js
@@ -6,15 +6,48 @@ const ContactUs = () => {
         action='https://formsubmit.co/reyesmikyle17@gmail.com'
         method='POST'
         className='container mx-auto w-8/12 flex flex-col gap-10'>
-        <div className='flex flex-row'>
-          <input type='text' name='firstName' placeholder='First Name' required />
-          <input type='text' name='lastName' placeholder='Last Name' required />
+        <div className='flex flex-row gap-24'>
+          <label htmlFor='firstName' className='w-full flex flex-col'>
+            First Name
+            <input type='text' name='firstName' placeholder='First Name' required />
+          </label>
+          <label htmlFor='lastName' className='w-full flex flex-col'>
+            Last Name
+            <input
+              type='text'
+              name='lastName'
+              placeholder='Last Name'
+              className='w-full'
+              required
+            />
+          </label>
         </div>
-        <div>
-          <input type='email' name='email' placeholder='email' />
-          <input type='text' name='company' placeholder='Company Name' required />
+        <div className='flex flex-row gap-24'>
+          <label htmlFor='email' className='w-full flex flex-col'>
+            Email Address
+            <input
+              type='email'
+              name='email'
+              placeholder='Email Address'
+              className='w-full'
+              required
+            />
+          </label>
+          <label htmlFor='company' className='w-full flex flex-col'>
+            Company Name
+            <input
+              type='text'
+              name='company'
+              placeholder='Company Name'
+              className='w-full'
+              required
+            />
+          </label>
         </div>
-        <textarea type='text' rows='4' cols='50' />
+        <label htmlFor='message' className='flex flex-col'>
+          Message
+          <textarea type='text' name='message' rows='4' cols='50' />
+        </label>
         <input type='hidden' name='_captcha' value='false' />
         <button type='submit'>Send</button>
       </form>
