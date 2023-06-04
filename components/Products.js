a3311b78bff4394deef84f682f3ede178c2f90d6
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-03 23:15:45 -0700

    Add responsiveness to the products header

--- components/Products.js
+++ components/Products.js
@@ -1,12 +1,10 @@
 const Products = () => {
   return (
-    <div className='h-[100vh]'>
-      <div className='flex flex-col gap-6 items-center container mx-auto'>
-        <div className='flex flex-col'>
-          <p className='text-5xl text-primary opacity-80'>Discover</p>
-          <p className='text-7xl pl-40 text-[#43759b]'>Our</p>
-          <p className='text-8xl pl-60 text-[#265D6A]'>Products</p>
-        </div>
+    <div className='h-full flex flex-col gap-10 md:gap-20 items-center container mx-auto mb-[100px]'>
+      <div className='flex flex-col items-center md:items-baseline'>
+        <p className='text-5xl md:text-6xl text-primary opacity-80'>Discover</p>
+        <p className='text-5xl md:text-6xl md:pl-40 text-[#43759b]'>Our</p>
+        <p className='text-5xl md:text-8xl md:pl-60 text-[#265D6A]'>Products</p>
       </div>
     </div>
   )
