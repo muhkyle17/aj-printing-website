e6db1e9e5c62f0b082c409adc07e97c2efc6f171
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-03 19:17:55 -0700

    Add responsiveness to our company values header

--- components/CompanyPhilosophy.js
+++ components/CompanyPhilosophy.js
@@ -4,11 +4,11 @@ import printingImage2 from '../public/print-image-2.jpeg'
 
 const CompanyPhilosophy = () => {
   return (
-    <div className='h-full flex flex-col gap-20 items-center container mx-auto'>
-      <div className='flex flex-col'>
-        <p className='text-5xl text-primary opacity-80'>Our</p>
-        <p className='text-8xl pl-10 mb-5 text-[#43759b]'>Company</p>
-        <p className='text-7xl pl-60 text-[#264D6A]'>Values</p>
+    <div className='h-full flex flex-col gap-10 md:gap-20 items-center container mx-auto'>
+      <div className='flex flex-col items-center md:items-baseline'>
+        <p className='text-6xl md:text-5xl text-primary opacity-80'>Our</p>
+        <p className='text-6xl sm:text-8xl md:pl-10 md:mb-5 text-[#43759b]'>Company</p>
+        <p className='text-6xl sm:text-7xl md:pl-60 text-[#264D6A]'>Values</p>
       </div>
       <div className='flex xl:flex-row flex-col items-center gap-2 w-full xl:mx-28'>
         <p className='text-base w-3/4 xl:w-1/4 text-primary opacity-80 leading-6'>
