9289223cd539f6546831f02f1c0c04b8d696f049
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-05 18:13:27 -0700

    Add input type styling

--- components/ContactUs.js
+++ components/ContactUs.js
@@ -5,51 +5,64 @@ const ContactUs = () => {
       <form
         action='https://formsubmit.co/reyesmikyle17@gmail.com'
         method='POST'
-        className='container mx-auto w-8/12 flex flex-col gap-10'>
-        <div className='flex flex-row gap-24'>
-          <label htmlFor='firstName' className='w-full flex flex-col'>
+        className='container mx-auto w-7/12 flex flex-col gap-10'>
+        <div className='flex flex-row gap-16'>
+          <label htmlFor='firstName' className='w-full flex flex-col gap-1'>
             First Name
-            <input type='text' name='firstName' placeholder='First Name' required />
+            <input
+              type='text'
+              name='firstName'
+              placeholder='First Name'
+              className='w-full rounded-xl py-2 px-3 border-2 border-gray-200'
+              required
+            />
           </label>
-          <label htmlFor='lastName' className='w-full flex flex-col'>
+          <label htmlFor='lastName' className='w-full flex flex-col gap-1'>
             Last Name
             <input
               type='text'
               name='lastName'
               placeholder='Last Name'
-              className='w-full'
+              className='w-full rounded-xl py-2 px-3 border-2 border-gray-200'
               required
             />
           </label>
         </div>
-        <div className='flex flex-row gap-24'>
-          <label htmlFor='email' className='w-full flex flex-col'>
+        <div className='flex flex-row gap-16'>
+          <label htmlFor='email' className='w-full flex flex-col gap-1'>
             Email Address
             <input
               type='email'
               name='email'
               placeholder='Email Address'
-              className='w-full'
+              className='w-full rounded-xl py-2 px-3 border-2 border-gray-200'
               required
             />
           </label>
-          <label htmlFor='company' className='w-full flex flex-col'>
+          <label htmlFor='company' className='w-full flex flex-col gap-1'>
             Company Name
             <input
               type='text'
               name='company'
               placeholder='Company Name'
-              className='w-full'
+              className='w-full rounded-xl py-2 px-3 border-2 border-gray-200'
               required
             />
           </label>
         </div>
-        <label htmlFor='message' className='flex flex-col'>
+        <label htmlFor='message' className='flex flex-col gap-1'>
           Message
-          <textarea type='text' name='message' rows='4' cols='50' />
+          <textarea
+            type='text'
+            name='message'
+            rows='4'
+            cols='50'
+            className='w-full rounded-xl py-2 px-3 border-2 border-gray-200'
+            placeholder='Leave us a message...'
+          />
         </label>
-        <input type='hidden' name='_captcha' value='false' />
         <button type='submit'>Send</button>
+        <input type='hidden' name='_captcha' value='false' />
       </form>
     </div>
   )
