636d9f59b422e22ab6b82ee6384b31ff7fff65d1
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-05 18:16:51 -0700

    Add typography styling changes

--- components/ContactUs.js
+++ components/ContactUs.js
@@ -6,58 +6,60 @@ const ContactUs = () => {
         action='https://formsubmit.co/reyesmikyle17@gmail.com'
         method='POST'
         className='container mx-auto w-7/12 flex flex-col gap-10'>
-        <div className='flex flex-row gap-16'>
-          <label htmlFor='firstName' className='w-full flex flex-col gap-1'>
+        <div className='flex flex-row gap-10'>
+          <label
+            htmlFor='firstName'
+            className='w-full flex flex-col gap-1 text-primary font-medium'>
             First Name
             <input
               type='text'
               name='firstName'
               placeholder='First Name'
-              className='w-full rounded-xl py-2 px-3 border-2 border-gray-200'
+              className='w-full rounded-xl py-2 px-3 border-2 border-gray-200 font-normal'
               required
             />
           </label>
-          <label htmlFor='lastName' className='w-full flex flex-col gap-1'>
+          <label htmlFor='lastName' className='w-full flex flex-col gap-1 text-primary font-medium'>
             Last Name
             <input
               type='text'
               name='lastName'
               placeholder='Last Name'
-              className='w-full rounded-xl py-2 px-3 border-2 border-gray-200'
+              className='w-full rounded-xl py-2 px-3 border-2 border-gray-200 font-normal'
               required
             />
           </label>
         </div>
-        <div className='flex flex-row gap-16'>
-          <label htmlFor='email' className='w-full flex flex-col gap-1'>
+        <div className='flex flex-row gap-10'>
+          <label htmlFor='email' className='w-full flex flex-col gap-1 text-primary font-medium'>
             Email Address
             <input
               type='email'
               name='email'
               placeholder='Email Address'
-              className='w-full rounded-xl py-2 px-3 border-2 border-gray-200'
+              className='w-full rounded-xl py-2 px-3 border-2 border-gray-200 font-normal'
               required
             />
           </label>
-          <label htmlFor='company' className='w-full flex flex-col gap-1'>
+          <label htmlFor='company' className='w-full flex flex-col gap-1 text-primary font-medium'>
             Company Name
             <input
               type='text'
               name='company'
               placeholder='Company Name'
-              className='w-full rounded-xl py-2 px-3 border-2 border-gray-200'
+              className='w-full rounded-xl py-2 px-3 border-2 border-gray-200 font-normal'
               required
             />
           </label>
         </div>
-        <label htmlFor='message' className='flex flex-col gap-1'>
+        <label htmlFor='message' className='flex flex-col gap-1 text-primary font-medium'>
           Message
           <textarea
             type='text'
             name='message'
             rows='4'
             cols='50'
-            className='w-full rounded-xl py-2 px-3 border-2 border-gray-200'
+            className='w-full rounded-xl py-2 px-3 border-2 border-gray-200 font-normal'
             placeholder='Leave us a message...'
           />
         </label>
