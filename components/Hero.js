3b789a43b93c047ce81251c9fd08aa87e3a70b48
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-10 00:20:12 -0700

    Adjust hero text to the left a little bit

--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,7 @@
 const Hero = () => {
   return (
     <section className='h-[100vh] flex items-center py-32 lg:py-0 overflow-hidden relative'>
-      <div className='lg:absolute z-10 px-24 container w-full sm:w-auto lg:bottom-[17rem] lg:right-[22rem] 2xl:right-[40vw] flex flex-col gap-2 text-primary text-center md:text-left text-2xl sm:text-4xl md:text-6xl xl:text-7xl'>
+      <div className='lg:absolute z-10 px-24 container w-full sm:w-auto lg:bottom-[17rem] lg:right-[30rem] 2xl:right-[40vw] flex flex-col gap-2 text-primary text-center md:text-left text-2xl sm:text-4xl md:text-6xl xl:text-7xl'>
         <span>Printing Power</span>
         <span className='font-extrabold sm:leading-[4.4rem]'>High Quality Labels</span>
         <span className='text-md md:text-lg xl:text-xl mt-5'>For Your Brand Needs.</span>
