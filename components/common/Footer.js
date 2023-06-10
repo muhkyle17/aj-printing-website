9edc7fd6f9283d21bec86a5e8f910854821a457f
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-10 00:10:56 -0700

    Fix facebook social in footer

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -63,7 +63,11 @@ const Footer = () => {
               </svg>
               +63 82 392 8776
             </p>
-            <p>
+            <a
+              href='https://www.facebook.com/NorthGraphicPrinting/'
+              target='_blank'
+              className='border border-white w-fit p-1.5 rounded-full'
+              rel='noreferrer'>
               <svg
                 id='fico'
                 xmlns='http://www.w3.org/2000/svg'
@@ -78,7 +82,7 @@ const Footer = () => {
                 class='feather feather-facebook'>
                 <path d='M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z' />
               </svg>
-            </p>
+            </a>
           </div>
         </div>
       </div>
