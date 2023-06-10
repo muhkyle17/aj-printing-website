232011eb5afee78bfd63394975ac6fadcfc6e255
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-10 13:32:53 -0700

    Initialize company page

---
+++ pages/company.js
@@ -0,0 +1,12 @@
+import Header from '../components/common/Header'
+
+const Company = () => {
+  return (
+    <>
+      <Header />
+      <p>Company Page</p>
+    </>
+  )
+}
+
+export default Company
