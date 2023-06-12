3a1fca7f3fc6b7af873822ff47cd27bbe68bbfb4
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-11 23:31:49 -0700

    Add head text and icon head to company page

--- pages/company.js
+++ pages/company.js
@@ -1,10 +1,16 @@
+import Head from 'next/head'
 import Header from '../components/common/Header'
+import Hero from '../components/Company/Hero'
 
 const Company = () => {
   return (
     <>
+      <Head>
+        <title>{'North Graphic Supplies'}</title>
+        <link rel='icon' href='/NGS.png' />
+      </Head>
       <Header />
-      <p>Company Page</p>
+      <Hero />
     </>
   )
 }
