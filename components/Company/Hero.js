e81dd2cd21f5b9706e3951f328ca76a8b9dcec25
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-12 00:32:36 -0700

    Add company page hero portion

--- components/Company/Hero.js
+++ components/Company/Hero.js
@@ -1,7 +1,19 @@
+import Image from 'next/image'
+import companyHeroImage from '../../public/hero-company-image.jpeg'
+
 const Hero = () => {
   return (
     <>
-      <p>Hero</p>
+      <div className='h-screen flex items-center justify-center gap-10 mx-auto container'>
+        <Image src={companyHeroImage} alt='Hero Image' className='w-auto rounded-3xl h-[70vh]' />
+        <div className='flex flex-col gap-5 text-primary text-right'>
+          <h2 className='text-7xl font-bold'>{`What we're about.`}</h2>
+          <h3 className='text-md'>
+            Powerful printing solutions driven by a talented team of workers, businessmen and <br />
+            printers located in the Philippines.
+          </h3>
+        </div>
+      </div>
     </>
   )
 }
