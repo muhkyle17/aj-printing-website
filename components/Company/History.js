27ff352f93ccbd890086cd7cfeb2a005163f053f
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-18 13:38:55 -0700

    Add responsiveness to company history component

--- components/Company/History.js
+++ components/Company/History.js
@@ -1,8 +1,8 @@
 const History = () => {
   return (
-    <div className='mx-auto container relative'>
+    <div className='mx-auto container relative mt-[5rem] md:mt-0'>
       <div className='text-primary flex flex-col gap-5 w-[85%] mb-44'>
-        <h3 className='text-5xl font-bold leading-[3.5rem] z-10'>
+        <h3 className='text-3xl sm:text-4xl md:text-5xl leading-10 md:leading-[3.5rem] font-bold z-10'>
           North Graphic Supplies was founded in 2008 as a small scale printing company, providing
           labels and prints for local businesses and start-ups alike
         </h3>
@@ -19,7 +19,7 @@ const History = () => {
         </p>
       </div>
       <div
-        className='absolute opacity-90 w-2/5 min-h-[24rem] bottom-[-70px] rounded-md right-[-90px] bg-cover bg-no-repeat bg-right'
+        className='hidden md:block absolute opacity-90 w-2/5 min-h-[24rem] bottom-[10px] lg:bottom-[-70px] rounded-md right-[-90px] bg-cover bg-no-repeat bg-right'
         style={{
           backgroundImage: `url('/company-founded-image.jpeg')`,
         }}
