4a1d6987d05127c06af674a25dca9bc5652e2d89
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-17 23:04:04 -0700

    Add responsiveness to hero component of company page

--- components/Company/Hero.js
+++ components/Company/Hero.js
@@ -4,10 +4,10 @@ import companyHeroImage from '../../public/hero-company-image.jpeg'
 const Hero = () => {
   return (
     <>
-      <div className='h-screen flex items-center justify-center gap-10 mx-auto container'>
+      <div className='h-screen mt-[150px] md:mt-0 flex flex-wrap-reverse md:flex-nowrap items-center justify-center gap-10 mx-auto container'>
         <Image src={companyHeroImage} alt='Hero Image' className='w-auto rounded-3xl h-[70vh]' />
-        <div className='flex flex-col gap-5 text-primary text-right'>
-          <h2 className='text-7xl font-bold'>{`What we're about.`}</h2>
+        <div className='flex flex-col gap-5 text-primary text-center md:text-right'>
+          <h2 className='text-4xl sm:text-7xl font-bold'>{`What we're about.`}</h2>
           <h3 className='text-md'>
             Powerful printing solutions driven by a talented team of workers, businessmen and <br />
             printers located in the Philippines.
