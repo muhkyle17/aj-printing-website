acdcdfd2ab0b3b309aa23fa063953d3da85712a4
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-18 00:09:58 -0700

    Fix responsiveness of image

--- components/Company/Hero.js
+++ components/Company/Hero.js
@@ -4,8 +4,12 @@ import companyHeroImage from '../../public/hero-company-image.jpeg'
 const Hero = () => {
   return (
     <>
-      <div className='h-screen mt-[150px] md:mt-0 flex flex-wrap-reverse md:flex-nowrap items-center justify-center gap-10 mx-auto container'>
-        <Image src={companyHeroImage} alt='Hero Image' className='w-auto rounded-3xl h-[70vh]' />
+      <div className='h-screen mt-[150px] md:mt-0 flex flex-wrap-reverse md:flex-nowrap items-center justify-center md:gap-10 mx-auto container'>
+        <Image
+          src={companyHeroImage}
+          alt='Hero Image'
+          className='w-auto rounded-3xl max-h-[70vh] self-end sm:self-center'
+        />
         <div className='flex flex-col gap-5 text-primary text-center md:text-right'>
           <h2 className='text-4xl sm:text-7xl font-bold'>{`What we're about.`}</h2>
           <h3 className='text-md'>
