e55ee21cd45811742e76db12841797765a82c548
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-25 19:19:59 -0700

    Add header and sub header for employees section

--- components/Company/People.js
+++ components/Company/People.js
@@ -1,7 +1,10 @@
 const People = () => {
   return (
-    <div>
-      <h2>People</h2>
+    <div className='flex flex-col gap-6 container mx-auto text-primary'>
+      <h2 className='text-8xl font-bold'>Our People.</h2>
+      <h3 className='text-2xl w-2/5 leading-10'>
+        The talented individuals behind every design and printer.
+      </h3>
     </div>
   )
 }
