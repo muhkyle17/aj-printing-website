c02de7b298bd22bb1622cec7fdd8fcd3afca0be6
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-06-27 23:59:50 -0700

    Add head tag and contents to products page

--- pages/products.js
+++ pages/products.js
@@ -1,9 +1,14 @@
+import Head from 'next/head'
 import Header from '../components/common/Header'
 import Footer from '../components/common/Footer'
 
 const products = () => {
   return (
     <>
+      <Head>
+        <title>{'North Graphic Supplies'}</title>
+        <link rel='icon' href='/NGS.png' />
+      </Head>
       <Header />
       <Footer />
     </>
