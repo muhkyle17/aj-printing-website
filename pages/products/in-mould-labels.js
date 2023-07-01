9c9897f5f3936bbfbd1e12efcf30c017f810dd9f
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-07-01 14:22:59 -0700

    Add image for in mould labels and import to hero component of in mould labels page

--- pages/products/in-mould-labels.js
+++ pages/products/in-mould-labels.js
@@ -1,9 +1,14 @@
 import Layout from '../../components/common/Layout'
+import ProductsHero from '../../components/Products/common/ProductsHero'
 
 const InMouldLabels = () => {
   return (
     <Layout>
-      <p>In mould labels</p>
+      <ProductsHero
+        backgroundImage='/in-mould-labels-hero.jpeg'
+        title='In Mould Labels'
+        description='Roll to roll labels by NGS will print out high quality printing labels for various applications such as product packaging, shipping labels, barcodes, and promotional stickers.'
+      />
     </Layout>
   )
 }
