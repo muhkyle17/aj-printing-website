e0dd001f023cbf6be109e77794abe9471fefe226
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-07-01 16:49:21 -0700

    Adjust some minor styling in the about component of each product

--- components/Products/common/About.js
+++ components/Products/common/About.js
@@ -2,12 +2,16 @@ import Image from 'next/image'
 
 const About = ({ header, description, image, height }) => {
   return (
-    <div className='container mx-auto mt-20 flex flex-col md:flex-row justify-center gap-10'>
+    <div className='container mx-auto mt-20 flex flex-col md:flex-row justify-center gap-10 mb-16'>
       <div className='w-full md:w-1/3 text-primary flex flex-col gap-5'>
         <h2 className='text-xl'>{header}</h2>
         <p>{description}</p>
       </div>
-      <Image src={image} alt='Product Type Image' className={`max-w-[30rem] w-auto ${height}`} />
+      <Image
+        src={image}
+        alt='Product Type Image'
+        className={`max-w-[30rem] rounded-lg w-auto ${height}`}
+      />
     </div>
   )
 }
