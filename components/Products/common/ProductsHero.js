b085842cc1354e3213000d10157c08727ed9d48c
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-07-01 12:54:02 -0700

    Render roll to roll product hero image along with the common component

---
+++ components/Products/common/ProductsHero.js
@@ -0,0 +1,28 @@
+const ProductsHero = ({ backgroundImage, title, description }) => {
+  console.log(backgroundImage)
+  return (
+    <div>
+      <div className='relative mt-[7.5rem]'>
+        <div
+          style={{ backgroundImage: `url('${backgroundImage}')` }}
+          className='w-[100vw] h-[60vh] mt-[7.5rem] bg-cover bg-no-repeat bg-center filter brightness-50 flex items-center justify-center'
+        />
+        <div
+          style={{
+            position: 'absolute',
+            top: '50%',
+            left: '50%',
+            transform: 'translate(-50%, -50%)',
+          }}
+          className='flex flex-col gap-5'>
+          <h2 className='text-7xl text-primaryBackground opacity-100 text-center w-[70vw]'>
+            {title}
+          </h2>
+          <p className='text-center text-3xl text-primaryBackground leading-10'>{description}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductsHero
