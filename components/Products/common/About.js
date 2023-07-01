5bd3897a9abb58b331f9f887b24733f335c6eb01
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-07-01 16:47:09 -0700

    Make products portion of each product responsive

--- components/Products/common/About.js
+++ components/Products/common/About.js
@@ -2,12 +2,12 @@ import Image from 'next/image'
 
 const About = ({ header, description, image, height }) => {
   return (
-    <div className='container mx-auto mt-20 flex flex-row justify-center gap-10'>
-      <div className='w-1/3 text-primary flex flex-col gap-5'>
+    <div className='container mx-auto mt-20 flex flex-col md:flex-row justify-center gap-10'>
+      <div className='w-full md:w-1/3 text-primary flex flex-col gap-5'>
         <h2 className='text-xl'>{header}</h2>
         <p>{description}</p>
       </div>
-      <Image src={image} alt='Product Type Image' className={`w-1/3 ${height}`} />
+      <Image src={image} alt='Product Type Image' className={`max-w-[30rem] w-auto ${height}`} />
     </div>
   )
 }
