f83edf92d56d9668596f39eef9aa99aa0d1ddf80
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-07-01 13:51:04 -0700

    Initialize sheeted labels and in mould labels pages

---
+++ pages/products/in-mould-labels.js
@@ -0,0 +1,11 @@
+import Layout from '../../components/common/Layout'
+
+const InMouldLabels = () => {
+  return (
+    <Layout>
+      <p>In mould labels</p>
+    </Layout>
+  )
+}
+
+export default InMouldLabels
---
+++ pages/products/sheeted-labels.js
@@ -0,0 +1,11 @@
+import Layout from '../../components/common/Layout'
+
+const SheetedLabels = () => {
+  return (
+    <Layout>
+      <p>Sheeted Labels</p>
+    </Layout>
+  )
+}
+
+export default SheetedLabels
