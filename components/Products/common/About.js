b21c2bf437bc07eaa3fd9f57df41870b1001c044
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-07-01 16:38:33 -0700

    Slightly adjust spacing and making height adjustable

--- components/Products/common/About.js
+++ components/Products/common/About.js
@@ -1,13 +1,13 @@
 import Image from 'next/image'
 
-const About = ({ header, description, image }) => {
+const About = ({ header, description, image, height }) => {
   return (
     <div className='container mx-auto mt-20 flex flex-row justify-center gap-10'>
-      <div className='w-1/3 text-primary'>
+      <div className='w-1/3 text-primary flex flex-col gap-5'>
         <h2 className='text-xl'>{header}</h2>
         <p>{description}</p>
       </div>
-      <Image src={image} alt='Product Type Image' className={`w-1/3 ${'max-h-[17rem]'}`} />
+      <Image src={image} alt='Product Type Image' className={`w-1/3 ${height}`} />
     </div>
   )
 }
