effb27de8a9a8c5937bc847c48b01c44ef9b5a51
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-07-01 10:19:38 -0700

    Import common and next head components for roll to roll labels page

--- pages/products/roll-to-roll.js
+++ pages/products/roll-to-roll.js
@@ -1,7 +1,16 @@
+import Head from 'next/head'
+import Header from '../../components/common/Header'
+import Footer from '../../components/common/Footer'
+
 const RollToRoll = () => {
   return (
     <>
-      <p>Roll to roll page</p>
+      <Head>
+        <title>North Graphic Supplies</title>
+        <link rel='icon' href='/NGS.png' />
+      </Head>
+      <Header />
+      <Footer />
     </>
   )
 }
