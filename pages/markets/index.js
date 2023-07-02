436721c44efed9376dcec66a642aa6cf3b0d488d
Mikyle Reyes <reyesmikyle17@gmail.com>
2023-07-02 10:30:03 -0700

    Add markets index page

--- pages/markets/index.js
+++ pages/markets/index.js
@@ -1,9 +1,18 @@
 import Layout from '../../components/common/Layout'
+import Hero from '../../components/Products/common/Hero'
+import Markets from '../../components/Home/Markets'
 
 const markets = () => {
   return (
     <Layout>
-      <p>Markets</p>
+      <Hero
+        backgroundImage='/market-index-hero.jpeg'
+        title='Markets'
+        description='NGS products that match your printing needs'
+      />
+      <div className='mt-24'>
+        <Markets />
+      </div>
     </Layout>
   )
 }
