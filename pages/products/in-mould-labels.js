a0eb6debc3df055a075b8a114a1d063acaed1ee8
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-01-27 22:31:29 -0800

    Fix the spelling of integration

--- pages/products/in-mould-labels.js
+++ pages/products/in-mould-labels.js
@@ -12,7 +12,7 @@ const InMouldLabels = () => {
         description='Roll to roll labels by NGS will print out high quality printing labels for various applications such as product packaging, shipping labels, barcodes, and promotional stickers.'
       />
       <About
-        header='In-Mould is the intergration of the label with the packaging during the injection.'
+        header='In-Mould is the integration of the label with the packaging during the injection.'
         description='In this process, the label is placed into the IML injection mould, then melted
           thermoplastic polymer combines with the IML label and takes the shape of the mould. Thus,
           production of packaging and labeling are performed at the same time. IML process can be
