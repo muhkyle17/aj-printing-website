d0cee70a74e0354d7587e834b46550254f38e8fd
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-01-28 00:18:51 -0800

    Add imports and initialize structure

---
+++ pages/markets/healthcare.js
@@ -0,0 +1,14 @@
+import Layout from '../../components/common/Layout'
+import Hero from '../../components/ProductsMarkets/common/Hero'
+import About from '../../components/ProductsMarkets/common/About'
+
+const Healthcare = () => {
+  return (
+    <Layout>
+      <Hero />
+      <About />
+    </Layout>
+  )
+}
+
+export default Healthcare
