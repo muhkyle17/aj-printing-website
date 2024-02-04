242b0b52e91577af11e8b15a672be793b7bccde3
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-02-04 11:37:49 -0800

    Slightly adjust the height of the image in barcodes

--- pages/markets/barcodes.js
+++ pages/markets/barcodes.js
@@ -15,7 +15,7 @@ const Barcodes = () => {
         header='We understand that the backbone of seamless inventory management, retail operations, and logistics lies in the clarity and accuracy of barcodes.'
         description='Elevate your product identification with our custom barcode labels. We offer a range of sizes, materials, and printing options to ensure your barcodes are not only functional but also align with your branding. Embrace the power of QR codes for interactive marketing, product information, and beyond. Our QR code printing services enable you to seamlessly integrate this versatile technology into your packaging and marketing materials.'
         image={BarcodesProduct}
-        height='h-[20rem]'
+        height='h-[23rem]'
       />
     </Layout>
   )
