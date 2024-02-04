28eb3659d077ab8c4f0d28e75f9ed120738ae14f
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-02-04 11:33:56 -0800

    Update the description and header of both the hero and the products portion of the barcodes page

--- pages/markets/barcodes.js
+++ pages/markets/barcodes.js
@@ -6,8 +6,17 @@ import BarcodesProduct from '../../public/barcodes-product.jpeg'
 const Barcodes = () => {
   return (
     <Layout>
-      <Hero />
-      <About />
+      <Hero
+        backgroundImage='/barcodes-hero.jpeg'
+        title='Barcodes'
+        description=' Our dedicated barcode printing services are tailored to meet the diverse needs of businesses seeking precision, reliability, and efficiency.'
+      />
+      <About
+        header='Barcodes hader about blah blah'
+        description='Elevate your product identification with our custom barcode labels. We offer a range of sizes, materials, and printing options to ensure your barcodes are not only functional but also align with your branding. Embrace the power of QR codes for interactive marketing, product information, and beyond. Our QR code printing services enable you to seamlessly integrate this versatile technology into your packaging and marketing materials.'
+        image={BarcodesProduct}
+        height='h-[20rem]'
+      />
     </Layout>
   )
 }
