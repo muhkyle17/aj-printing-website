837d9b39c560aa81678e6f5adbc06bf1ae92d6a9
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-02-04 12:14:20 -0800

    Code the ui and titles and description of the chemicals page

--- pages/markets/chemicals.js
+++ pages/markets/chemicals.js
@@ -1,8 +1,23 @@
+import Layout from '../../components/common/Layout'
+import Hero from '../../components/ProductsMarkets/common/Hero'
+import About from '../../components/ProductsMarkets/common/About'
+import ChemicalsProduct from '../../public/chemicals-product.jpeg'
+
 const Chemicals = () => {
   return (
-    <div>
-      <p>Chemicals</p>
-    </div>
+    <Layout>
+      <Hero
+        backgroundImage='/chemicals-hero.jpeg'
+        title='Chemicals'
+        description='Innovating Labels for Chemical Excellence'
+      />
+      <About
+        header='We recognize that the chemical industry demands more than just labels; it requires a meticulous approach that aligns with regulatory standards and communicates critical information effectively.'
+        description='Our chemicals printing services are designed to meet these exacting needs, providing reliable and compliant labeling solutions for a wide range of chemical products. We ensure that your chemical products are labeled in accordance with international standards, promoting safety and compliance.'
+        image={ChemicalsProduct}
+        height='h-[23rem]'
+      />
+    </Layout>
   )
 }
 
