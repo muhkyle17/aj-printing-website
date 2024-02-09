1a8ed419216734686d8879c8d7d846ff89689066
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-02-08 23:26:32 -0800

    Separate the description into 3 paragraphs

--- components/ProductsMarkets/common/About.js
+++ components/ProductsMarkets/common/About.js
@@ -1,11 +1,13 @@
 import Image from 'next/image'
 
-const About = ({ header, description, image, height }) => {
+const About = ({ header, description1, description2, description3, image, height }) => {
   return (
     <div className='container mx-auto mt-20 flex flex-col md:flex-row justify-center gap-10 mb-16'>
       <div className='w-full md:w-1/3 text-primary flex flex-col gap-5'>
         <h2 className='text-xl'>{header}</h2>
-        <p>{description}</p>
+        <p className='tracking-wide'>{description1}</p>
+        <p className='tracking-wide'>{description2}</p>
+        <p className='tracking-wide'>{description3}</p>
       </div>
       <Image
         src={image}
