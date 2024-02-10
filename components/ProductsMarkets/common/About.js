32b0d050efef552de6bb5379d2470c8885ed9782
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-02-10 12:20:49 -0800

    Remove a bit of gap between the descriptions

--- components/ProductsMarkets/common/About.js
+++ components/ProductsMarkets/common/About.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 const About = ({ header, description1, description2, description3, image, height }) => {
   return (
     <div className='container mx-auto mt-20 flex flex-col md:flex-row justify-center gap-10 mb-16'>
-      <div className='w-full md:w-1/3 text-primary flex flex-col gap-5'>
+      <div className='w-full md:w-1/3 text-primary flex flex-col gap-4'>
         <h2 className='text-xl'>{header}</h2>
         <p className='tracking-wide'>{description1}</p>
         <p className='tracking-wide'>{description2}</p>
