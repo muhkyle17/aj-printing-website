e4f6bd03334dfcfc932ea0a97f48e13f7016efc4
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-02-10 12:34:54 -0800

    Fix the description of the barcodes market product portion

--- pages/markets/barcodes.js
+++ pages/markets/barcodes.js
@@ -13,9 +13,11 @@ const Barcodes = () => {
       />
       <About
         header='We understand that the backbone of seamless inventory management, retail operations, and logistics lies in the clarity and accuracy of barcodes.'
-        description='Elevate your product identification with our custom barcode labels. We offer a range of sizes, materials, and printing options to ensure your barcodes are not only functional but also align with your branding. Embrace the power of QR codes for interactive marketing, product information, and beyond. Our QR code printing services enable you to seamlessly integrate this versatile technology into your packaging and marketing materials.'
+        description1='Elevate your product identification with our custom barcode labels.'
+        description2='We offer a range of sizes, materials, and printing options to ensure your barcodes are not only functional but also align with your branding. Embrace the power of QR codes for interactive marketing, product information, and beyond.'
+        description3='Our QR code printing services enable you to seamlessly integrate this versatile technology into your packaging and marketing materials.'
         image={BarcodesProduct}
-        height='h-[23rem]'
+        height='h-[24.5rem]'
       />
     </Layout>
   )
