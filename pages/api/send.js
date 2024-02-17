528e880dbd2f9263e8c1ae4af75960b2f90ceb1b
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-02-16 23:57:41 -0700

    Create send email apit ourte

---
+++ pages/api/send.js
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { EmailTemplate } from '../../components/EmailTemplate'
+import { Resend } from 'resend'
+
+const resend = new Resend(process.env.RESEND_API_KEY)
+console.log(resend, 'resend')
