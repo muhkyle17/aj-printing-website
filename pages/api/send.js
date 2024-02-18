a759c09f4ebd986907851d5858ed5e821bb6b4e2
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-02-17 22:55:57 -0700

    Add the code for the backend call to resend

--- pages/api/send.js
+++ pages/api/send.js
@@ -1,6 +1,24 @@
+/* eslint-disable import/no-anonymous-default-export */
 import { NextApiRequest, NextApiResponse } from 'next'
-import { EmailTemplate } from '../../components/EmailTemplate'
+import { EmailTemplate } from '../../components/Resend/email-template'
 import { Resend } from 'resend'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 console.log(resend, 'resend')
+
+export default async (NextApiRequest, NextApiResponse) => {
+  const { data, error } = await resend.emails.send({
+    from: 'Mikyle <sales@northgraphicsupplies.com>',
+    to: ['reyesmikyle17@gmail.com'],
+    subject: 'Test',
+    react: EmailTemplate({ firstName: 'Kyle' }),
+  })
+
+  if (error) {
+    return NextApiResponse.json(error)
+  }
+
+  console.log(data, 'data')
+
+  NextApiResponse.status(200).json(data)
+}
